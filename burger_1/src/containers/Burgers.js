a7c45dfdeaafe3d895577d43532fbf49b3dac0eb
Thien Vu <vuthienktb@gmail.com>
2021-04-19 22:35:52 +0700

    firebase and create spinner

--- burger_1/src/containers/Burgers.js
+++ burger_1/src/containers/Burgers.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import Board from '../components/Board/Board'
 import Burger from '../components/Burger/Burger'
 import Modal from '../components/Modal/Modal';
-import Content from '../components/Modal/Content/Content'
+import Content from '../components/Modal/Content/Content';
+import axios from '../axios';
+import Spinner from '../components/Spinner/Spinner';
 
 const Burgers = () => {
     
@@ -15,7 +17,8 @@ const [burgerElements, setBurgerElements] = useState({
     meat: NUMBER_DEFAULT
 });
 const [totalPrice, setTotalPrice] = useState(TOTALPRICE_DEFAULT);
-const [purchase, setPurchase] = useState(false)
+const [purchase, setPurchase] = useState(false);
+const [loading, setLoading] = useState(false);
 
 const burgerElementPrice={
     salad: 0.5,
@@ -28,6 +31,20 @@ const showModal= () => {
 }
 const closeModal= () =>{
     setPurchase(false)
+}
+const continueOrder = () => {
+    setLoading(true);
+    const data = {
+        burgerElements, totalPrice
+    }
+    axios.post('/orders.json', data).then((res)=> {
+        setLoading(false);
+        setPurchase(false);        
+        console.log(res)
+    }).catch(err =>{
+        setLoading(false);
+        setPurchase(false)
+    });
 }
 const addElementToBurger = (type) => {
     const qty = burgerElements[type];
@@ -58,12 +75,16 @@ const removeElementFromBurger = (type) => {
     setBurgerElements(newBurgerElements);
     setTotalPrice(newTotalPrice);
 }
+let contentOrder = <Content burgerElements={burgerElements} totalPrice={totalPrice} cancelClick={closeModal} continueClick={continueOrder} />;
+if (loading) {
+    contentOrder = <Spinner />
+}
     return (
         <>
             <Burger burgerElements={burgerElements} />
             <Board totalPrice={totalPrice} showModal={showModal} addBurgerElement={addElementToBurger} removeBurgerElement={removeElementFromBurger}/>
             <Modal show={purchase} close={closeModal}>
-                <Content burgerElements={burgerElements} totalPrice={totalPrice} btnClick={closeModal}/>
+               {contentOrder}
             </Modal>
         </>
     )
